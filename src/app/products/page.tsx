0b4aa458fb2eaa9c205eab240dbf0cce01aed340
unknown <brunoprotz81@gmail.com>
2024-10-17 18:20:01 +0200

    Fixed errors about boundry

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,9 +4,17 @@ import { useSearchParams  } from 'next/navigation';
 import SectionTitle from "@/components/Common/SectionTitle";
 import SingleProduct from "@/components/Products/SingleProduct";
 import productsData from "@/components/Products/productsData";
+import { Suspense } from 'react'
+
+function Search() {
+  const searchParams = useSearchParams()
+  const format = searchParams.get('format');
+  const page = searchParams.get('page');
+  return format;
+}
 
 const Products = () => {
-  const searchParams = useSearchParams()
+  /* const searchParams = useSearchParams() */
   const [hash, setHash] = useState('');
 
   useEffect(() => {
@@ -14,15 +22,18 @@ const Products = () => {
     setHash(window.location.hash);
   }, []);
 
-  const format = searchParams.get('format');
-  const page = searchParams.get('page');
+  /* const format = searchParams.get('format');
+  const page = searchParams.get('page'); */
 
   return (
     <section id="products" className="dark:bg-gray-dark py-16 md:py-20 lg:py-28">
         <div className="container">
           <div className="w-full max-w-[800px] mb-[50px]">
             <p className="text-base !leading-relaxed text-body-color md:text-lg uppercase">
-              /HOME/CATEGORY/SUBCATEGORy
+              /HOME/CATEGORY/
+              <Suspense>
+                <Search />
+              </Suspense>
             </p>
             <h2 className="mb-4 text-3xl uppercase font-bold !leading-tight text-amber-500 dark:text-body-color-dark sm:text-4xl md:text-[45px]">
               Products
