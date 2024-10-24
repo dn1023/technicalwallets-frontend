2516cf5b3a57b780c29b464b4eb91cf90cfdbcd4
unknown <brunoprotz81@gmail.com>
2024-10-24 16:50:07 +0200

    Add hero mp4

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -104,15 +104,15 @@ const Hero = () => {
           {/* <h2 className="text-white text-2xl font-bold">"Our 3D Animation"</h2> */}
           {/* <Image src="/images/favicon_large.png" alt="Picture of the hero" className="" width={500} height={500} /> */}
           <div className="max-w-[800px]">
-            <div className="w-full text-center text-black uppercase font-bold text-[28px] backdrop-blur-sm bg-white/70 p-3">
+            <div className="w-full text-center text-black uppercase font-bold text-[28px] backdrop-blur-sm bg-white/70 p-5">
               OUR COLLABORATIONS
             </div>
-            <div className="uppercase w-full text-center px-5 py-10 font-bold leading-tight backdrop-blur-sm text-black dark:text-white md:text-amber-500 sm:leading-tight md:leading-tight">
+            {/* <div className="w-full text-center px-5 py-10 font-bold leading-tight backdrop-blur-sm text-black dark:text-white md:text-amber-500 sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
               Techincal Wallet is not only an Architectural and Interior Design Studio, but also a 3D Visualization  and Virtual Reality Studio co-producing 3D imagery and animations with reputable architects, real estate developers, and product-marketing agencies.
             </div>
             <div className="w-full text-center text-black uppercase font-bold text-[28px] backdrop-blur-sm bg-white/70 p-3">
               Welcome to Our Service!
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
